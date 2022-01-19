6895b101e4230cd39c1cc41dd4186bf68da02304
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-19 20:14:09 +0400

    update brand and consumer route hero background col

--- src/routes/brands/index.svelte
+++ src/routes/brands/index.svelte
@@ -3,4 +3,28 @@
   <title>Brands</title>
 </svelte:head>
 
-<h1>Brands Landing Page</h1>
+<div class="hero min-h-screen">
+  <div class="flex-col justify-center hero-content lg:flex-row">
+    <div class="text-center lg:text-left">
+      <h1 class="mb-5 text-5xl font-bold">
+            TRUBIT for Brands
+          </h1>
+      <p class="mb-5">
+            Your Web3 portal to interact with existing customer in novel ways and reach new audiences.
+          </p>
+    </div>
+    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Signin or Signup</span>
+          </label>
+          <input type="text" placeholder="Email" class="input input-bordered">
+        </div>
+        <div class="form-control mt-6">
+          <input type="button" value="Get Magic Link" class="btn btn-primary">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/consumers/index.svelte
+++ src/routes/consumers/index.svelte
@@ -3,4 +3,16 @@
   <title>Consumers</title>
 </svelte:head>
 
-<h1>Consumers Landing Page</h1>
+<div class="hero min-h-screen">
+  <div class="text-center hero-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">
+            TRUBIT Wallet
+          </h1>
+      <p class="mb-5">
+            Take ownership of your data and earn bitcoin.
+          </p>
+      <button class="btn btn-primary">Connect wallet</button>
+    </div>
+  </div>
+</div>
