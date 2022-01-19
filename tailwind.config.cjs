6127b9d6e56d52f2961663d7762c0cb49845de0c
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-19 20:34:36 +0400

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,23 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+      minHeight: {
+        '1/2': '50%',
+        '3/4': '75%',
+      }
+    },
+
 	},
 
 	plugins: [
     require('@tailwindcss/typography'),
     require('daisyui'),
-  ]
+  ],
+
+  daisyui: {
+
+  }
 };
 
 module.exports = config;
