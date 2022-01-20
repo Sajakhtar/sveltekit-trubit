e6a3bd8dc545b3babb8b587051cce9495a9996a4
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-20 11:41:28 +0400

    add theme-change to layout files, put darmod button its own component, use data-theme-toggle for light and dark themes

--- src/routes/brands/__layout.reset.svelte
+++ src/routes/brands/__layout.reset.svelte
@@ -2,6 +2,12 @@
 	import '../../app.css';
   import Footer from '$lib/Footer.svelte'
   import Nav from '$lib/Nav.svelte'
+  import { onMount } from 'svelte'
+  import { themeChange } from 'theme-change'
+
+  onMount(async () => {
+    themeChange(false)
+  })
 </script>
 
 <div class="container mx-auto my-4">
