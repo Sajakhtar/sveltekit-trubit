770826f6b9285ffdc1173bac7130fc992a6b33e0
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-28 10:24:19 +0400

    add brands/accoun/ routes

---
+++ src/routes/brands/account/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Sidebar from '$lib/Sidebar.svelte'
+  import { browser } from '$app/env'
+  import { goto } from '$app/navigation'
+  import Error from '$lib/Error.svelte'
+  import { getUser, signOut } from '$lib/auth'
+
+  // const user = getUser()
+  // if (browser && !user) goto('/brands')
+
+</script>
+
+
+<Sidebar />
