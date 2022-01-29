526ff35e71a9a8d437c7c9ac30abe11b98c236c7
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-29 13:33:09 +0400

    add rounds for brand account audiences and campaigns

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -44,7 +44,7 @@
         </a>
       </li>
       <li>
-        <a ref="/brands/audiences">
+        <a href="/brands/account/audiences">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill mr-4" viewBox="0 0 16 16">
             <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
@@ -54,7 +54,7 @@
         </a>
       </li>
       <li>
-        <a ref="/brands/campaigns">
+        <a href="/brands/account/campaigns">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images mr-4" viewBox="0 0 16 16">
             <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
             <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
---
+++ src/routes/brands/account/audiences.svelte
@@ -0,0 +1,13 @@
+<script>
+  // import Sidebar from '$lib/Sidebar.svelte'
+  import { browser } from '$app/env'
+  import { goto } from '$app/navigation'
+  import Error from '$lib/Error.svelte'
+  import { getUser, signOut } from '$lib/auth'
+
+  // const user = getUser()
+  // if (browser && !user) goto('/brands')
+
+</script>
+
+<h1>Audiences</h1>
