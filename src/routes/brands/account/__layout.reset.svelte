c31aa8ec63e68ffc5f6e76fee2822aded066eca0
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-31 17:07:19 +0400

    remove unneccessary imports

--- src/routes/brands/account/__layout.reset.svelte
+++ src/routes/brands/account/__layout.reset.svelte
@@ -3,6 +3,14 @@
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
 
+  import { browser } from '$app/env'
+  import { goto } from '$app/navigation'
+  import Error from '$lib/Error.svelte'
+  import { getUser } from '$lib/auth'
+
+  // const user = getUser()
+  // if (browser && !user) goto('/brands')
+
 
   onMount(async () => {
     themeChange(false)
--- src/routes/brands/account/audiences.svelte
+++ src/routes/brands/account/audiences.svelte
@@ -1,12 +1,4 @@
 <script>
-  // import Sidebar from '$lib/Sidebar.svelte'
-  import { browser } from '$app/env'
-  import { goto } from '$app/navigation'
-  import Error from '$lib/Error.svelte'
-  import { getUser, signOut } from '$lib/auth'
-
-  // const user = getUser()
-  // if (browser && !user) goto('/brands')
 
 </script>
 
