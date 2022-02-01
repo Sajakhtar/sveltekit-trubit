528599d0414f36c247ebcb0405743782306d1da7
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-01 17:58:35 +0400

    search filter: display categories on focus, no on blur, set category to search when clicked

--- src/lib/SearchFilter.svelte
+++ src/lib/SearchFilter.svelte
@@ -1,6 +1,7 @@
 <script>
   export let dataCategories
   let search = ""
+  let isFocused = false
 
   $: filteredDataCategories = dataCategories.filter((category) => {
     return search === "" || category.toLowerCase().indexOf(search.toLowerCase()) !== -1
@@ -12,11 +13,13 @@
   <label class="label">
     <span class="label-text">Audience attributes</span>
   </label>
-  <input bind:value={search} placeholder="Search" class="input input-bordered w-full" type="text">
+  <input on:focus={() => isFocused = true} on:blue={() => isFocused = false} bind:value={search} placeholder="Search" class="input input-bordered w-full" type="text">
 </div>
 
-<ul>
-  {#each filteredDataCategories as dataCategory}
-    <li>{dataCategory}</li>
-  {/each}
-</ul>
+{#if isFocused}
+  <ul>
+    {#each filteredDataCategories as dataCategory}
+      <li on:click={() => search = dataCategory}>{dataCategory}</li>
+    {/each}
+  </ul>
+{/if}
