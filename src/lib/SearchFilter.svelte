d75b6ac72abe00b0952228af14d7b466e314ead9
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-01 17:00:14 +0400

    modify searchFilter component to display filtered list based on search term

--- src/lib/SearchFilter.svelte
+++ src/lib/SearchFilter.svelte
@@ -1,17 +1,22 @@
 <script>
   export let dataCategories
+  let search = ""
+
+  $: filteredDataCategories = dataCategories.filter((category) => {
+    return search === "" || category.toLowerCase().indexOf(search.toLowerCase()) !== -1
+  })
 </script>
 
 
 <div>
   <label class="label">
-    <span class="label-text">Search audience attributes</span>
+    <span class="label-text">Audience attributes</span>
   </label>
-  <input placeholder="search" class="input input-bordered w-full" type="text">
+  <input bind:value={search} placeholder="Search" class="input input-bordered w-full" type="text">
 </div>
 
 <ul>
-  {#each dataCategories as dataCategory}
+  {#each filteredDataCategories as dataCategory}
     <li>{dataCategory}</li>
   {/each}
 </ul>
