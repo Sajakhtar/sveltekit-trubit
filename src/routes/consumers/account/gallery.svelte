509768f75028c297d76afc6a7320bc69d617dd0c
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-04 20:20:27 +0400

    add consumer gallery route with a card

---
+++ src/routes/consumers/account/gallery.svelte
@@ -0,0 +1,17 @@
+
+<main class="container mx-auto my-4">
+  <h1 class="text-5xl mb-8">NFT Gallery</h1>
+
+  <div class="card shadow-xl image-full">
+    <figure>
+      <img src="https://picsum.photos/id/1005/400/250">
+    </figure>
+    <div class="justify-end card-body">
+      <h2 class="card-title">Image overlay</h2>
+      <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
+      <div class="card-actions">
+        <button class="btn btn-primary">Reveal</button>
+      </div>
+    </div>
+  </div>
+</main>
--- src/routes/consumers/account/index.svelte
+++ src/routes/consumers/account/index.svelte
@@ -1,1 +1,5 @@
-<h1 class="text-5xl mb-8">Consumer Account</h1>
+
+<main class="container mx-auto my-4">
+  <h1 class="text-5xl mb-8">Consumer Account</h1>
+  <a href="/consumers/account/gallery" class="btn btn-primary">My NFTs</a>
+</main>
