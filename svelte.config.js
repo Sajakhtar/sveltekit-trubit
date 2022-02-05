05430fa9eb012fefe0b63021dd26ceb204332176
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-05 10:30:00 +0400

    svelte config: add vite config to try fix regen runtime error

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,24 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+    adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+    // Possible solution to regenerator runtime issue
+    // https://stackoverflow.com/questions/70097108/sveltekit-could-not-resolve-dependency-even-though-its-external
+    vite: {
+      ssr: {
+        // noExternal: ['@stacks/connect', '@stacks/profile', '@stacks/storage']
+        // noExternal: []
+        external: ['@stacks/connect', '@stacks/profile', '@stacks/storage']
+      },
+      optimizeDeps: {
+      //   include: ['@stacks/connect', '@stacks/profile', '@stacks/storage']
+        exclude: ['@stacks/connect', '@stacks/profile', '@stacks/storage']
+      }
+    },
 	},
 
 	preprocess: [preprocess({})]
